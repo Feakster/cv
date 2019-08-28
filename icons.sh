f3d798f329b854a9e3d0bc2be66b94a8cef646eb
Benjamin Feakins <greatwhite@posteo.eu>
2019-08-28 13:42:43 +0100

    Added HTTP status code checks to the icons.sh script.

--- icons.sh
+++ icons.sh
@@ -22,34 +22,82 @@ fi
 
 ### Get Vector Graphics ###
 echo 'Downloading icons ...'
+## Dimensions Icon ##
 if [ ! -f "./$DIR/dimensions.svg" ]
 then
-  curl -s https://38h6q83kpel22aipe0iux4i1-wpengine.netdna-ssl.com/wp-content/themes/dimensions/public/images/logo.svg > "./$DIR/dimensions.svg" # Dimensions
+  URL=https://38h6q83kpel22aipe0iux4i1-wpengine.netdna-ssl.com/wp-content/themes/dimensions/public/images/logo.svg
+  if curl -Is $URL | head -n 1 | grep "200" > /dev/null
+  then
+    curl -s $URL > "./$DIR/dimensions.svg"
+  else
+    echo "Dimensions icon not found"
+    exit 1
+  fi
 fi
 
+## Email Icon ##
 if [ ! -f "./$DIR/email.svg" ]
 then
-  curl -s http://www.clker.com/cliparts/0/6/8/3/12065629431871551574qubodup_16x16px-capable_black_and_white_icons_10.svg > "./$DIR/email.svg" # Email
+  URL=http://www.clker.com/cliparts/0/6/8/3/12065629431871551574qubodup_16x16px-capable_black_and_white_icons_10.svg
+  if curl -Is $URL | head -n 1 | grep "200" > /dev/null
+  then
+    curl -s $URL > "./$DIR/email.svg"
+  else
+    echo "Email icon not found"
+    exit 1
+  fi
 fi
 
+## GitHub Icon ##
 if [ ! -f "./$DIR/github.svg" ]
 then
-  curl -s https://image.flaticon.com/icons/svg/25/25231.svg > "./$DIR/github.svg" # GitHub
+  URL=https://image.flaticon.com/icons/svg/25/25231.svg
+  if curl -Is $URL | head -n 1 | grep "200" > /dev/null
+  then
+    curl -s $URL > "./$DIR/github.svg"
+  else
+    echo "GitHub icon not found"
+    exit 1
+  fi
 fi
 
+## Phone Icon ##
 if [ ! -f "./$DIR/phone.svg" ]
 then
-  curl -s http://cdn.onlinewebfonts.com/svg/download_247097.svg > "./$DIR/phone.svg" # Phone
+  URL=http://cdn.onlinewebfonts.com/svg/download_247097.svg
+  if curl -Is $URL | head -n 1 | grep "200" > /dev/null
+  then
+    curl -s $URL > "./$DIR/phone.svg"
+  else
+    echo "Phone icon not found"
+    exit 1
+  fi
 fi
 
+## Twitter Icon ##
 if [ ! -f "./$DIR/twitter.svg" ]
 then
-  curl -s https://image.flaticon.com/icons/svg/8/8800.svg > "./$DIR/twitter.svg" # Twitter
+  URL=https://image.flaticon.com/icons/svg/8/8800.svg
+  if curl -Is $URL | head -n 1 | grep "200" > /dev/null
+  then
+    curl -s $URL > "./$DIR/twitter.svg"
+  else
+    echo "Twitter icon not found"
+    exit 1
+  fi
 fi
 
+## Website Icon ##
 if [ ! -f "./$DIR/website.svg" ]
 then
-  curl -s https://openclipart.org/download/216096/WWW-Icon.svg > "./$DIR/website.svg" # Website
+  URL=https://openclipart.org/download/216096/WWW-Icon.svg
+  if curl -Is $URL | head -n 1 | grep "200" > /dev/null
+  then
+    curl -s $URL > "./$DIR/website.svg"
+  else
+    echo "Website icon not found"
+    exit 1
+  fi
 fi
 
 ### Convert SVG to PDF ###
