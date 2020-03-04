6b6e9bda9dcf65e7447525c2b8e4930e59c0c4e1
Feakster <greatwhite@posteo.eu>
2020-03-04 15:47:51 +0000

    Julian's paper got published
    - Added a year to Julian's paper in the cv.tex file.
    - Added a DOI to Julian's paper and the Altmetric badge extraction script.

--- badges.R
+++ badges.R
@@ -19,7 +19,8 @@ doi <- list(
   "10.1016/j.jnucmat.2018.04.022",
   "10.1136/bmj.k2387",
   "10.1136/bmjopen-2017-019637",
-  "10.12688/f1000research.20229.1"
+  "10.12688/f1000research.20229.1",
+  "10.3399/bjgpopen20X101016"
 )
 
 ### Define Altmetric Data Extraction Function ###
