39f69167b8ddb80e6f35604af4b30c466ed8c796
Feakster <feakster@posteo.eu>
2020-12-17 13:40:25 +0000

    Boby & Iryna's paper was published
    - Moved Bobby & Iryna's paper to the published section.
    - Marked the shell scripts as executable.
    - Added the DOI of the published paper to the badges.R script.

--- badges.R
+++ badges.R
@@ -19,7 +19,8 @@ doi <- list(
   "10.1136/bmj.k2387",
   "10.1136/bmjopen-2017-019637",
   "10.12688/f1000research.20229.1",
-  "10.3399/bjgpopen20x101016"
+  "10.3399/bjgpopen20x101016",
+  "10.1371/journal.pmed.1003478"
 )
 
 ### Define Altmetric Data Extraction Function ###
